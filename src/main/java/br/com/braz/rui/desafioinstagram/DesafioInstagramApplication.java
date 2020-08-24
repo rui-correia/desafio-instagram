37738ce1bfc48c14c874212a6c596dc74090d925
Rui Braz <rui.braz.correia@gmail.com>
2020-08-24 20:22:38 -0300

    Primeiro commit, projeto criado.

---
+++ src/main/java/br/com/braz/rui/desafioinstagram/DesafioInstagramApplication.java
@@ -0,0 +1,13 @@
+package br.com.braz.rui.desafioinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/braz/rui/desafioinstagram/DesafioInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.braz.rui.desafioinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
